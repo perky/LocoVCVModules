6b591b6806b7c81af682a2ec76f96b950eab7468
Luke <luke.perkin@gmail.com>
2020-01-18 19:57:22 +0000

    add TEX and CHAOS modules

---
+++ src/Chaos.cpp
@@ -0,0 +1,270 @@
+#include "plugin.hpp"
+#include "osdialog.h"
+#include <vector>
+
+#define VOLT_MAX 10.f
+#define VOLT_FIVE 5.f
+#define POLY_CHANNELS 16
+#define PI 3.14159265359
+
+typedef unsigned int uint;
+typedef unsigned char uchar;
+
+struct ChaosModule : Module {
+	uint frameIndex = 0;
+	uint channelCount = 1;
+	
+	dsp::SchmittTrigger kickTrigger;
+
+	enum ParamIds {
+		GRAVITY_PARAM,
+		LENGTH_RATIO_PARAM,
+		DAMPING_PARAM,
+		KICK_PARAM,
+		NUM_PARAMS
+	};
+	enum InputIds {
+		GRAVITY_IN,
+		RATIO_IN,
+		DAMPING_IN,
+		KICK_TRIG_IN,
+		NUM_INPUTS
+	};
+	enum OutputIds {
+		POLY_CHAOS_OUTPUT,
+		NUM_OUTPUTS
+	};
+	enum LightIds {
+		NUM_LIGHTS
+	};
+
+	struct Pendulum {
+		float theta = 0.0f;
+		float length = 0.5f;
+		float vel = 0.0f;
+		float acc = 0.0f;
+		float mass = 10.0f;
+		float x, y = 0.f;
+	};
+
+	Pendulum p0;
+	Pendulum p1;
+	int frame = 0;
+
+	ChaosModule() {
+		config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
+		configParam(GRAVITY_PARAM, 0.01f, 4.f, 1.0f, "timewarp", "x");
+		configParam(LENGTH_RATIO_PARAM, 0.1f, 1.0f - 0.1f, 0.5f, "ratio", "");
+		configParam(DAMPING_PARAM, 0.0f, 1.f, 0.0f, "dampen", "");
+		configParam(KICK_PARAM, 0.f, 1.f, 0.f);
+	}
+
+	json_t *dataToJson() override {
+		json_t *obj = json_object();
+		return obj;
+	}
+
+	void dataFromJson(json_t *rootJ) override {
+		
+	}
+
+	float pixelToVoltage(uchar pixel) {
+		return ((float)pixel / 255) * VOLT_MAX;
+	}
+
+	void kickPendulums() {
+		p0.theta = 2.5f + (random::uniform() * 0.1f);
+		p1.theta = 0.25f + (random::uniform() * 0.1f);
+	}
+
+	#define THETA_1 0
+	#define VEL_1 1
+	#define THETA_2 2
+	#define VEL_2 3
+
+	void process(const ProcessArgs& args) override {
+        // Audio signals are typically +/-5V
+        // https://vcvrack.com/manual/VoltageStandards.html
+		frame++;
+
+		if (frame % 4 == 0) {
+			const float g = 9.81f;
+			float length_ratio = params[LENGTH_RATIO_PARAM].getValue() + (inputs[RATIO_IN].getVoltageSum() / VOLT_MAX);
+			clamp(length_ratio, 0.01f , 0.99f);
+			float dt = args.sampleTime * (params[GRAVITY_PARAM].getValue() + (inputs[GRAVITY_IN].getVoltageSum() / VOLT_MAX));
+			clamp(dt, 0.f, 4.f);
+			float damping_in = (params[DAMPING_PARAM].getValue() + inputs[DAMPING_IN].getVoltageSum());
+			float damping = (damping_in > 0.1f) ? 0.99999f : 1.0f;
+
+			if (kickTrigger.process(inputs[KICK_TRIG_IN].getVoltage() + params[KICK_PARAM].getValue()))
+			{
+				kickPendulums();
+			}
+
+			p0.length = length_ratio;
+			p1.length = 1.0f - length_ratio;
+			p0.mass = p0.length * 10.f;
+			p1.mass = p1.length * 10.f;
+
+			const float deltaTheta = p0.theta - p1.theta;
+			const float mass_sum = p0.mass + p1.mass;
+			const float shared_denominator = (2.f * mass_sum - p1.mass * std::cos(2.f * p0.theta - 2.f * p1.theta));
+			const float p0_numerator = 
+				-g * (2.f * mass_sum) * std::sin(p0.theta) 
+				- p1.mass * g * std::sin(p0.theta - 2.f * p1.theta) 
+				- 2.f * std::sin(deltaTheta) * p1.mass 
+				* ((p1.vel * p1.vel) * p1.length + (p0.vel * p0.vel) * p0.length * std::cos(deltaTheta));
+			const float p0_denominator = p0.length * shared_denominator;
+			p0.acc = p0_numerator / p0_denominator;
+
+			const float p1_numerator = 
+				2.f * std::sin(deltaTheta) * ((p0.vel * p0.vel) * p0.length * mass_sum 
+				+ g * mass_sum * std::cos(p0.theta) 
+				+ (p1.vel * p1.vel) * p1.length * p1.mass * std::cos(deltaTheta));
+			const float p1_denominator = p1.length * shared_denominator;
+			p1.acc = p1_numerator / p1_denominator;
+
+			p0.vel += p0.acc * dt;
+			p1.vel += p1.acc * dt;
+			p0.vel *= damping;
+			p1.vel *= damping;
+			p0.theta += p0.vel * dt;
+			p1.theta += p1.vel * dt;
+			if (p0.theta > PI*2) 
+				p0.theta -= PI*2;
+			if (p0.theta < PI*2)
+				p0.theta += PI*2;
+			if (p1.theta > PI * 2)
+				p1.theta -= PI * 2;
+			if (p1.theta < PI * 2)
+				p1.theta += PI * 2;
+
+			// Output pendulum 1 x position.
+			p0.x = 0.0f + (p0.length * std::sin(p0.theta));
+			p0.y = 0.0f - (p0.length * std::cos(p0.theta)) * -1.0f;
+			p1.x = p0.x + (p1.length * std::sin(p1.theta));
+			p1.y = p0.y - (p1.length * std::cos(p1.theta)) * -1.0f;
+			outputs[POLY_CHAOS_OUTPUT].setVoltage(p1.x * VOLT_FIVE, 0);
+			outputs[POLY_CHAOS_OUTPUT].setVoltage(p1.y * VOLT_FIVE, 1);
+
+			float theta1 = std::fmod(p0.theta + PI, PI*2);
+			if (theta1 < 0) theta1 += PI*2;
+			theta1 -= PI;
+
+			float theta2 = std::fmod(p1.theta + PI, PI * 2);
+			if (theta2 < 0)
+				theta2 += PI * 2;
+			theta2 -= PI;
+
+			outputs[POLY_CHAOS_OUTPUT].setVoltage((theta1 / PI) * VOLT_FIVE, 2);
+			outputs[POLY_CHAOS_OUTPUT].setVoltage((theta2 / PI) * VOLT_FIVE, 3);
+			outputs[POLY_CHAOS_OUTPUT].setVoltage(p0.vel, 4);
+			outputs[POLY_CHAOS_OUTPUT].setVoltage(p1.vel, 5);
+			outputs[POLY_CHAOS_OUTPUT].setChannels(6);
+			
+		}
+	}
+};
+
+struct PendulumWidget : OpaqueWidget {
+	ChaosModule* module;
+
+	void draw(const DrawArgs &args) override {
+		OpaqueWidget::draw(args);
+		if (module) {
+			const float maxLen = 80.0f;
+			const float center = 105.f;
+			float p0_x = (module->p0.x * maxLen) + center;
+			float p0_y = (module->p0.y * maxLen) + center;
+			float p1_x = (module->p1.x * maxLen) + center;
+			float p1_y = (module->p1.y * maxLen) + center;
+
+			nvgBeginPath(args.vg);
+			nvgStrokeWidth(args.vg, 1);
+			nvgStrokeColor(args.vg, nvgRGBA(0xED, 0x1B, 0x31, 0xFF));
+			nvgMoveTo(args.vg, center, center);
+			nvgLineTo(args.vg, p0_x, p0_y);
+			nvgCircle(args.vg, p0_x, p0_y, module->p0.mass);
+			nvgClosePath(args.vg);
+			nvgStroke(args.vg);
+
+			nvgBeginPath(args.vg);
+			nvgStrokeWidth(args.vg, 1);
+			nvgStrokeColor(args.vg, nvgRGBA(0x7E, 0xD3, 0xEF, 0xFF));
+			nvgMoveTo(args.vg, p0_x, p0_y);
+			nvgLineTo(args.vg, p1_x, p1_y);
+			nvgCircle(args.vg, p1_x, p1_y, module->p1.mass);
+			nvgClosePath(args.vg);
+			nvgStroke(args.vg);
+		}
+	}
+};
+
+struct ChaosWidget : ModuleWidget {
+	ChaosWidget(ChaosModule* module) {
+		setModule(module);
+		setPanel(APP->window->loadSvg(asset::plugin(pluginInstance, "res/Chaos.svg")));
+
+		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, 0)));
+		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, 0)));
+		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+
+		const float col01_x = 44.9;
+		const float col02_x = 112.5;
+		const float col03_x = 182.4;
+
+		const float row01_y = 72.2;
+		const float row02_y = 105.8;
+
+		const float kick_x = 36.8;
+		const float kick_y = 343.2;
+		const float kick_btn_x = 68.7;
+		const float kick_btn_y = 343.2;
+
+		const float poly_out_x = 187.8;
+		const float poly_out_y = 345.2;
+
+		const float scope_x = 7.5;
+		const float scope_y = 122.0;
+
+		addParam(createParamCentered<RoundBlackKnob>((Vec(col01_x, row01_y)), module, ChaosModule::GRAVITY_PARAM));
+		addParam(createParamCentered<CKD6>((Vec(col02_x, row01_y)), module, ChaosModule::DAMPING_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>((Vec(col03_x, row01_y)), module, ChaosModule::LENGTH_RATIO_PARAM));
+		addParam(createParamCentered<CKD6>((Vec(kick_btn_x, kick_btn_y)), module, ChaosModule::KICK_PARAM));
+
+		addInput(createInputCentered<PJ301MPort>((Vec(col01_x, row02_y)), module, ChaosModule::GRAVITY_IN));
+		addInput(createInputCentered<PJ301MPort>((Vec(col02_x, row02_y)), module, ChaosModule::RATIO_IN));
+		addInput(createInputCentered<PJ301MPort>((Vec(col03_x, row02_y)), module, ChaosModule::DAMPING_IN));
+		addInput(createInputCentered<PJ301MPort>((Vec(kick_x, kick_y)), module, ChaosModule::KICK_TRIG_IN));
+
+		addOutput(createOutputCentered<PJ301MPort>((Vec(poly_out_x, poly_out_y)), module, ChaosModule::POLY_CHAOS_OUTPUT));
+		
+
+		{
+			PendulumWidget *display = new PendulumWidget();
+			display->module = module;
+			display->box.pos = Vec(scope_x, scope_y);
+			display->box.size = Vec(210, 210);
+			addChild(display);
+		}
+	}
+
+	struct ChaosModuleItem : MenuItem {
+		ChaosModule *module;
+		void onAction(const event::Action &e) override {
+			MenuItem::onAction(e);
+			module->kickPendulums();
+		}
+	};
+
+	void appendContextMenu(ui::Menu *menu) override {
+		ChaosModule *module = dynamic_cast<ChaosModule*>(this->module);
+		assert(module);
+		menu->addChild(construct<MenuLabel>());
+		menu->addChild(construct<ChaosModuleItem>(&MenuItem::text, "Kick", &ChaosModuleItem::module, module));
+	}
+};
+
+
+Model* modelChaos = createModel<ChaosModule, ChaosWidget>("Chaos");
---
+++ src/Tex.cpp
@@ -0,0 +1,347 @@
+#include "plugin.hpp"
+#include "dep/lodepng/lodepng.h"
+#include "osdialog.h"
+#include <vector>
+
+#define IMG_WIDTH 256
+#define NUM_IMG_CHANNELS 3
+#define VOLT_MAX 10.f
+#define POLY_CHANNELS 16
+
+typedef unsigned int uint;
+typedef unsigned char uchar;
+
+struct TexModule : Module {
+	float pixels[IMG_WIDTH * IMG_WIDTH * NUM_IMG_CHANNELS];
+	uint pixelIndex[POLY_CHANNELS];
+	std::string lastImagePath;
+	bool bImageLoaded = false;
+	dsp::BooleanTrigger autoMode;
+	dsp::SchmittTrigger autoTrigger;
+	uint frameIndex = 0;
+	bool bAutoMode = true;
+	uint channelCount = 1;
+
+	struct PixelCoord {
+		float x, y = 0;
+	};
+	PixelCoord pixelNormalCoords[POLY_CHANNELS];
+
+	enum ParamIds {
+		X_OFFSET,
+		Y_OFFSET,
+		AUTO,
+		NUM_PARAMS
+	};
+	enum InputIds {
+		X_INPUT,
+		Y_INPUT,
+		TRIG_INPUT,
+		NUM_INPUTS
+	};
+	enum OutputIds {
+		RED_OUTPUT,
+		GREEN_OUTPUT,
+		BLUE_OUTPUT,
+		HUE_OUTPUT,
+		SATURATION_OUTPUT,
+		LEVEL_OUTPUT,
+		NUM_OUTPUTS
+	};
+	enum LightIds {
+		AUTO_LIGHT,
+		NUM_LIGHTS
+	};
+
+	TexModule() {
+		config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
+		configParam(X_OFFSET, 0.f, VOLT_MAX, 0.f, "x offset", "volts");
+		configParam(Y_OFFSET, 0.f, VOLT_MAX, 0.f, "y offset", "volts");
+		configParam(AUTO, 0.f, 1.f, 0.f);
+	}
+
+	json_t *dataToJson() override {
+		json_t *obj = json_object();
+		json_object_set_new(obj, "lastImagePath", json_string(lastImagePath.c_str()));
+		json_object_set_new(obj, "autoMode", json_integer((int)bAutoMode));
+		return obj;
+	}
+
+	void dataFromJson(json_t *rootJ) override {
+		json_t *lastImagePathJ = json_object_get(rootJ, "lastImagePath");
+		if (lastImagePathJ) {
+			lastImagePath = json_string_value(lastImagePathJ);
+			loadImage(lastImagePath);
+		}
+		json_t* autoModeJ = json_object_get(rootJ, "autoMode");
+		if (autoModeJ)
+			bAutoMode = json_integer_value(autoModeJ);
+	}
+
+	float pixelToVoltage(uchar pixel) {
+		return ((float)pixel / 255) * VOLT_MAX;
+	}
+
+	void loadImage(std::string path) {
+		std::vector<uchar> uncroppedImage;
+		uint uncroppedImageWidth;
+		uint uncroppedImageHeight;
+		uint error = lodepng::decode(uncroppedImage, uncroppedImageWidth, uncroppedImageHeight, path, LCT_RGB);
+		if (error != 0) {
+			std::cout << "error " << error << ": " << lodepng_error_text(error) << std::endl;
+			lastImagePath = "";
+			bImageLoaded = false;
+		} else {
+			for (uint y = 0; y < IMG_WIDTH; y++) {
+				for (uint x = 0; x < IMG_WIDTH; x++) {
+					const uint croppedIndex = ((y * IMG_WIDTH) + x) * NUM_IMG_CHANNELS;
+					if (x < uncroppedImageWidth && y < uncroppedImageHeight) {
+						const uint uncroppedIndex = ((y * uncroppedImageWidth) + x) * NUM_IMG_CHANNELS;
+						pixels[croppedIndex + 0] = pixelToVoltage(uncroppedImage[uncroppedIndex + 0]);
+						pixels[croppedIndex + 1] = pixelToVoltage(uncroppedImage[uncroppedIndex + 1]);
+						pixels[croppedIndex + 2] = pixelToVoltage(uncroppedImage[uncroppedIndex + 2]);
+					} else {
+						pixels[croppedIndex + 0] = 0.f;
+						pixels[croppedIndex + 1] = 0.f;
+						pixels[croppedIndex + 2] = 0.f;
+					}
+				}
+			}
+			lastImagePath = path;
+			bImageLoaded = true;
+		}
+	}
+
+	void process(const ProcessArgs& args) override {
+        // Audio signals are typically +/-5V
+        // https://vcvrack.com/manual/VoltageStandards.html
+
+		if (bImageLoaded) {
+
+			frameIndex++;
+
+			int xInChannelCount = std::max(inputs[X_INPUT].getChannels(), 1);
+			int yInChannelCount = std::max(inputs[Y_INPUT].getChannels(), 1);
+			channelCount = std::max(xInChannelCount, yInChannelCount);
+
+			if(autoMode.process(params[AUTO].getValue() > 0.f)) {
+				bAutoMode = !bAutoMode;
+			}
+
+			if (bAutoMode) {
+				bool bTrigger = (frameIndex % 2 == 0);
+				if (inputs[TRIG_INPUT].isConnected()) {
+					float trigValue = inputs[TRIG_INPUT].getVoltage();
+					bTrigger = autoTrigger.process(rescale(trigValue, 0.1f, 2.f, 0.f, 1.f));
+				}
+				if (bTrigger) {
+					const uint channel = 0;
+					pixelIndex[channel] = (pixelIndex[channel] + NUM_IMG_CHANNELS) % (IMG_WIDTH*IMG_WIDTH*NUM_IMG_CHANNELS);
+					const uint index1d = (pixelIndex[channel]/NUM_IMG_CHANNELS);
+					pixelNormalCoords[channel].x = (index1d % IMG_WIDTH) / (float)IMG_WIDTH;
+					pixelNormalCoords[channel].y = std::floor(index1d / (float)IMG_WIDTH) / (float)IMG_WIDTH;
+				}
+			} else {
+				float xOffset = params[X_OFFSET].getValue();
+				float yOffset = params[Y_OFFSET].getValue();
+				for (uint channel = 0; channel < channelCount; channel++) {
+					float xParam = inputs[X_INPUT].getNormalVoltage(0.f, channel);
+					float yParam = inputs[Y_INPUT].getNormalVoltage(0.f, channel);
+					xParam = clamp(xParam + xOffset, 0.f, VOLT_MAX) / VOLT_MAX;
+					yParam = clamp(yParam + yOffset, 0.f, VOLT_MAX) / VOLT_MAX;
+					pixelNormalCoords[channel].x = xParam;
+					pixelNormalCoords[channel].y = yParam;
+					uint xCoord = xParam * ((float)IMG_WIDTH);
+					uint yCoord = yParam * ((float)IMG_WIDTH);
+					pixelIndex[channel] = ((yCoord * IMG_WIDTH) + xCoord) * NUM_IMG_CHANNELS;
+				}
+			}
+			
+			for (uint channel = 0; channel < channelCount; channel++) {
+				float red = pixels[pixelIndex[channel] + 0];
+				float green = pixels[pixelIndex[channel] + 1];
+				float blue = pixels[pixelIndex[channel] + 2];
+				outputs[RED_OUTPUT].setVoltage(red, channel);
+				outputs[GREEN_OUTPUT].setVoltage(green, channel);
+				outputs[BLUE_OUTPUT].setVoltage(blue, channel);
+
+				float redNorm = red / VOLT_MAX;
+				float greenNorm = green / VOLT_MAX;
+				float blueNorm = blue / VOLT_MAX;
+				float cMax = std::max(redNorm, std::max(greenNorm, blueNorm));
+				float cMin = std::min(redNorm, std::min(greenNorm, blueNorm));
+				float cDelta = cMax - cMin;
+
+				float level = (0.299 * redNorm) + (0.587 * greenNorm) + (0.114 * blueNorm);
+				float saturation = 0.f;
+				if (cMax != cMin) {
+					if (level < 0.5f) {
+						saturation = (cMax - cMin) / (cMax + cMin);
+					} else {
+						saturation = (cMax - cMin) / (2.0f - cMax - cMin);
+					}
+				}
+
+				float hue = 0.f;
+				if (cMax != cMin && saturation > 0.f) {
+					if (cMax == redNorm) {
+						hue = (greenNorm - blueNorm) / cDelta;
+					} else if (cMax == greenNorm) {
+						hue = 2.f + (blueNorm - redNorm) / cDelta;
+					} else if (cMax == blueNorm) {
+						hue = 4.f + (redNorm - greenNorm) / cDelta;
+					}
+					hue *= 60.f;
+					if (hue > 0.f) {
+						hue = std::floor(hue);
+					} else {
+						hue = std::floor(360.f - hue);
+					}
+				}
+
+				outputs[HUE_OUTPUT].setVoltage((hue / 360.f) * VOLT_MAX, channel);
+				outputs[SATURATION_OUTPUT].setVoltage(saturation * VOLT_MAX, channel);
+				outputs[LEVEL_OUTPUT].setVoltage(level * VOLT_MAX, channel);	
+			}
+
+			for (uint outIndex = 0; outIndex < NUM_OUTPUTS; ++outIndex) {
+				outputs[outIndex].setChannels(channelCount);
+			}
+
+			lights[AUTO_LIGHT].setBrightness(bAutoMode ? 1.f : 0.0f);
+		}
+	}
+};
+
+
+struct TexModuleImageDisplay : OpaqueWidget {
+	TexModule* module;
+	std::string imagePath = "";
+	int imageWidth;
+	int imageHeight;
+	int imageHandle = 0;
+	bool bLoadedImage = false;
+	
+
+	void draw(const DrawArgs &args) override {
+		OpaqueWidget::draw(args);
+		if (module) {
+			if (module->bImageLoaded && (imagePath != module->lastImagePath)) {
+				imageHandle = nvgCreateImage(args.vg, module->lastImagePath.c_str(), 0);
+				imagePath = module->lastImagePath;
+				nvgImageSize(args.vg, imageHandle, &imageWidth, &imageHeight);
+			}
+
+			nvgBeginPath(args.vg);
+			//nvgScale(args.vg, 0.89843f, 0.89843f);
+			nvgScissor(args.vg, 0, 0, IMG_WIDTH, IMG_WIDTH);
+		 	NVGpaint imgPaint = nvgImagePattern(args.vg, 0, 0, imageWidth, imageHeight, 0, imageHandle, 1.0f);
+		 	nvgRect(args.vg, 0, 0, imageWidth, imageHeight);
+		 	nvgFillPaint(args.vg, imgPaint);
+		 	nvgFill(args.vg);
+			nvgClosePath(args.vg);
+		}
+	}
+};
+
+struct TexModuleCrosshair : OpaqueWidget {
+	TexModule* module;
+
+	void draw(const DrawArgs &args) override {
+		OpaqueWidget::draw(args);
+		if (module) {
+			const float size = IMG_WIDTH;
+			nvgBeginPath(args.vg);
+			nvgStrokeWidth(args.vg, 1);
+			nvgStrokeColor(args.vg, nvgRGBA(0xED, 0x1B, 0x31, 0xFF));
+			for (uint channel = 0; channel < module->channelCount; channel++) {
+				nvgMoveTo(args.vg, 0.f, module->pixelNormalCoords[channel].y * size);
+				nvgLineTo(args.vg, size, module->pixelNormalCoords[channel].y * size);
+				nvgMoveTo(args.vg, module->pixelNormalCoords[channel].x * size, 0.f);
+				nvgLineTo(args.vg, module->pixelNormalCoords[channel].x * size, size);
+			}
+			nvgClosePath(args.vg);
+			nvgStroke(args.vg);
+		}
+	}
+};
+
+struct TexModuleWidget : ModuleWidget {
+	TexModuleWidget(TexModule* module) {
+		setModule(module);
+		setPanel(APP->window->loadSvg(asset::plugin(pluginInstance, "res/Tex.svg")));
+
+		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, 0)));
+		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, 0)));
+		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+
+		const float col01_x = 47.0;
+		const float col02_x = 102.3;
+		const float row01_y = 78.8;
+		const float row02_y = 113.7;
+		const float row03_y = 162.5;
+		const float row04_y = 242.3;
+		const float row05_y = 284.0;
+		const float row06_y = 325.6;
+		const float img_x = 151.3;
+		const float img_y = 62.0;
+
+		addInput(createInputCentered<PJ301MPort>((Vec(col01_x, row01_y)), module, TexModule::X_INPUT));
+		addInput(createInputCentered<PJ301MPort>((Vec(col02_x, row01_y)), module, TexModule::Y_INPUT));
+
+		addParam(createParamCentered<RoundBlackKnob>((Vec(col01_x, row02_y)), module, TexModule::X_OFFSET));
+		addParam(createParamCentered<RoundBlackKnob>((Vec(col02_x, row02_y)), module, TexModule::Y_OFFSET));
+
+		addParam(createParamCentered<CKD6>(Vec(col01_x, row03_y), module, TexModule::AUTO));
+		addInput(createInputCentered<PJ301MPort>((Vec(col02_x, row03_y)), module, TexModule::TRIG_INPUT));
+		addChild(createLight<SmallLight<GreenLight>>(Vec(col01_x + 15, row03_y - 10), module, TexModule::AUTO_LIGHT));
+
+		addOutput(createOutputCentered<PJ301MPort>((Vec(col01_x, row04_y)), module, TexModule::HUE_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>((Vec(col01_x, row05_y)), module, TexModule::SATURATION_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>((Vec(col01_x, row06_y)), module, TexModule::LEVEL_OUTPUT));
+
+		addOutput(createOutputCentered<PJ301MPort>((Vec(col02_x, row04_y)), module, TexModule::RED_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>((Vec(col02_x, row05_y)), module, TexModule::GREEN_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>((Vec(col02_x, row06_y)), module, TexModule::BLUE_OUTPUT));
+
+		{
+			TexModuleImageDisplay *display = new TexModuleImageDisplay();
+			display->module = module;
+			display->box.pos = Vec(img_x, img_y);
+			display->box.size = Vec(256, 256);
+			addChild(display);
+		}
+
+		{
+			TexModuleCrosshair *display = new TexModuleCrosshair();
+			display->module = module;
+			display->box.pos = Vec(img_x, img_y);
+			display->box.size = Vec(256, 256);
+			addChild(display);
+		}
+	}
+
+	struct TexModuleItem : MenuItem {
+		TexModule *module;
+		void onAction(const event::Action &e) override {
+			MenuItem::onAction(e);
+			std::string dir = module->lastImagePath.empty() ?  asset::user("") : rack::string::directory(module->lastImagePath);
+			char *path = osdialog_file(OSDIALOG_OPEN, dir.c_str(), NULL, NULL);
+			if (path) {
+				module->loadImage(path);
+				free(path);
+			}
+		}
+	};
+
+	void appendContextMenu(ui::Menu *menu) override {
+		TexModule *module = dynamic_cast<TexModule*>(this->module);
+		assert(module);
+		menu->addChild(construct<MenuLabel>());
+		menu->addChild(construct<TexModuleItem>(&MenuItem::text, "Load image (png)", &TexModuleItem::module, module));
+	}
+};
+
+
+Model* modelTex = createModel<TexModule, TexModuleWidget>("Tex");
